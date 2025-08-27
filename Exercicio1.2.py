fa0395a67503b3b829e0c186451becbd029e032a
vitoria <vmelo3578@gmail.com>
2025-08-27 19:54:10 -0300

    Update Exercicio1.2.py

--- Exercicio1.2.py
+++ Exercicio1.2.py
@@ -22,4 +22,41 @@ while True:
     elif opcao == "2":
         print("nenhum evento cadastrado ainda!")
     else:
-        retirar = int(input())
+        retirar = int(input("Digite a quantidade a retirar:"))
+        if retirar <= 0:
+            print("A quantidade deve ser maior que zero!")
+        elif retirar > quantidade:
+            print("quantidade insuficiente no estoque!")
+        else:
+            quantidade -= retirar
+           print(f"Retirado{retirar} unidade(s). Estoque atual: {Quantidade}")
+                
+    elif opcao == "3":
+        if ingresso is None
+          print("Nenhum ingresso cadastrado ainda!")
+       else:
+            adicionar = int(input(Digite a quantidade a adicionar"))
+            if adicionar <= 0:
+                print ("A quantidade deve ser maior que zero!")
+            else:
+                quantidade += adicionar
+                print(f"Adicionado {adicionar} unidade(s). Estoque atual: {quantidade}")
+                
+     elif opcao == "4":
+         if ingresso is None:
+            print("Nenhum ingresso cadastrado ainda!")
+            else:
+                print(f"Ingresso: {ingresso} Quantidade em estoque:{quantidade}")
+                          
+                elif opcao == "4":
+                    print("saindo do sistema...até mais!")
+                    break
+                
+                else:
+                    print("Opção inválida! tente novamente.")
+                    
+                        
+        
+        
+        
+            
